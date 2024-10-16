f2fa488dfddba58036b43803880843605b33c467
TheBeard-af <afaqnadeem1993@gmail.com>
2024-10-16 12:15:51 +1100

    Update scripts.js
    Standardize the 'types' property across all objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 let pokemonList = [
-    { name: "pichu", height: 0.3, types: 'electric' },
+    { name: "pichu", height: 0.3, types: ['electric'] },
     { name: "Bulbasaur", height: 0.7, types: ['grass', 'poison'] },
-    { name: "squrtle", height: 0.5, types: 'water' }    
+    { name: "squrtle", height: 0.5, types: ['water'] }    
 ];
