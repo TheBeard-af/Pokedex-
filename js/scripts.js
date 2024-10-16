942644dfdc0dedadb15cb9cc8acc72bb614c8366
TheBeard-af <afaqnadeem1993@gmail.com>
2024-10-16 11:52:24 +1100

    pokemon list
    First 3 items of the pokidex

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
-alert('Hello world');
-let favoriteFood = "steak";
-document.write(favoriteFood);
+let pokemonList = [
+    { name: "pichu", height: 0.3, types: 'electric' },
+    { name: "Bulbasaur", height: 0.7, types: ['grass', 'poison'] },
+    { name: "squrtle", height: 0.5, types: 'water' }    
+];
