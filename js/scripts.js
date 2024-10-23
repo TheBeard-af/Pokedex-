24e6890dd3486685763d2063ec1c42a08cc773c9
TheBeard-af <afaqnadeem1993@gmail.com>
2024-10-23 11:35:13 +1100

    Added conditionals and loops
    Added loops and conditionals to display pokelist

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,15 @@
 let pokemonList = [
-    { name: "pichu", height: 0.3, types: ['electric'] },
+    { name: "Pichu", height: 0.3, types: ['electric'] },
     { name: "Bulbasaur", height: 0.7, types: ['grass', 'poison'] },
-    { name: "squrtle", height: 0.5, types: ['water'] }    
+    { name: "Squrtle", height: 0.5, types: ['water'] }    
 ];
+
+// for loop iterating over pokemon list
+
+for (let i = 0; i < pokemonList.length; i++) {
+    document.write(pokemonList[i].name + " " + "(height: " + pokemonList[i].height + ")");
+    if (pokemonList[i].height > 0.5) {
+        document.write("Wow, that's big!")
+    }
+    document.write ("<BR>")
+}
