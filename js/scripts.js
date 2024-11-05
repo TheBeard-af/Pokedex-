290ad452c5bd994b0c8c3beca6409d8229d383c6
TheBeard-af <afaqnadeem1993@gmail.com>
2024-11-06 10:39:28 +1100

    Changed for loop to for each ()

--- js/scripts.js
+++ js/scripts.js
@@ -6,10 +6,20 @@ let pokemonList = [
 
 // for loop iterating over pokemon list
 
-for (let i = 0; i < pokemonList.length; i++) {
-    document.write(pokemonList[i].name + " " + "(height: " + pokemonList[i].height + ")");
-    if (pokemonList[i].height > 0.5) {
+// for (let i = 0; i < pokemonList.length; i++) {
+//     document.write(pokemonList[i].name + " " + "(height: " + pokemonList[i].height + ")");
+//     if (pokemonList[i].height > 0.5) {
+//         document.write("Wow, that's big!")
+//     }
+//     document.write ("<BR>")
+// }
+
+// for each () loop iterating over pokemon list
+
+pokemonList.forEach(function(pokemon) {
+    document.write(pokemon.name + " " + "(height: " + pokemon.height + ") ");
+    if (pokemon.height > 0.5) {
         document.write("Wow, that's big!")
     }
     document.write ("<BR>")
-}
+  });
