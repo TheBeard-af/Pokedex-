4f3b0654e53ccae81810f1bd0817fa4ec31573ac
TheBeard-af <afaqnadeem1993@gmail.com>
2024-11-06 11:00:20 +1100

    IIFE added
    Encompassed the Pokemon list in an IIFE. Updated for each() loop to access IIFE.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,18 @@
+let pokemonRepository = (function () {
 let pokemonList = [
     { name: "Pichu", height: 0.3, types: ['electric'] },
     { name: "Bulbasaur", height: 0.7, types: ['grass', 'poison'] },
     { name: "Squrtle", height: 0.5, types: ['water'] }    
 ];
+return {    
+    getAll: function() {
+        return pokemonList;
+      },
+    add: function(pokemon) {
+      pokemonList.push(pokemon);
+    }
+  };
+})();
 
 // for loop iterating over pokemon list
 
@@ -15,8 +25,7 @@ let pokemonList = [
 // }
 
 // for each () loop iterating over pokemon list
-
-pokemonList.forEach(function(pokemon) {
+pokemonRepository.getAll().forEach(function(pokemon) {
     document.write(pokemon.name + " " + "(height: " + pokemon.height + ") ");
     if (pokemon.height > 0.5) {
         document.write("Wow, that's big!")
