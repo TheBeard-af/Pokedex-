1d19d10a85bf811e2343f3c3a9f4b8945a499711
TheBeard-af <afaqnadeem1993@gmail.com>
2024-11-11 10:32:20 +1100

    IIFE update
    Added contents of for each loop to IIFE and called it using a function. added event listener on button.

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,62 @@
 let pokemonRepository = (function () {
-let pokemonList = [
+  let pokemonList = [
     { name: "Pichu", height: 0.3, types: ['electric'] },
     { name: "Bulbasaur", height: 0.7, types: ['grass', 'poison'] },
-    { name: "Squrtle", height: 0.5, types: ['water'] }    
-];
-return {    
-    getAll: function() {
-        return pokemonList;
-      },
-    add: function(pokemon) {
-      pokemonList.push(pokemon);
-    }
+    { name: "Squrtle", height: 0.5, types: ['water'] }
+  ];
+
+  function getAll() {
+    return pokemonList;
+  }
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+  function addListItem(pokemon) {
+    let newlist = document.querySelector(".pokemon-list");
+    let listpokemon = document.createElement("li");
+    let button = document.createElement("button");
+    button.innerText = pokemon.name;
+    button.classList.add("button-class");
+    listpokemon.appendChild(button);
+    newlist.appendChild(listpokemon);
+    button.addEventListener('click', showDetails(pokemon){
+      console.log(pokemon);  
+    });
+  }
+  function showDetails(pokemon){
+    console.log(pokemon)    
+  }
+
+  return {
+    add: add,
+    getAll: getAll,
+    addListItem: addListItem,
+    showDetails: showDetails
   };
 })();
 
-// for loop iterating over pokemon list
+pokemonRepository.getAll().forEach(function (pokemon) {
+  pokemonRepository.addListItem(pokemon);
+});
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for loop iterating over pokemon list instead of for each()
 // for (let i = 0; i < pokemonList.length; i++) {
 //     document.write(pokemonList[i].name + " " + "(height: " + pokemonList[i].height + ")");
 //     if (pokemonList[i].height > 0.5) {
@@ -25,10 +66,3 @@ return {
 // }
 
 // for each () loop iterating over pokemon list
-pokemonRepository.getAll().forEach(function(pokemon) {
-    document.write(pokemon.name + " " + "(height: " + pokemon.height + ") ");
-    if (pokemon.height > 0.5) {
-        document.write("Wow, that's big!")
-    }
-    document.write ("<BR>")
-  });
