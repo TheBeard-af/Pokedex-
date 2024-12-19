86cfabddf0027216cd2304e3f4242e712979641a
TheBeard-af <afaqnadeem1993@gmail.com>
2024-12-19 13:38:15 +1100

    updated button

--- js/scripts.js
+++ js/scripts.js
@@ -68,7 +68,7 @@ let pokemonRepository = (function () {
 
 		let button = document.createElement('button');
 		button.innerText = pokemon.name;
-		button.classList.add('btn', 'btn-primary', 'w-100');
+		button.classList.add('btn', 'btn-primary', 'w-30');
 		button.addEventListener('click', function () {
 			showDetails(pokemon);
 		});
@@ -76,37 +76,6 @@ let pokemonRepository = (function () {
 		listItem.appendChild(button);
 		pokemonListElement.appendChild(listItem);
 	}
-
-  
-
-  // function showModal(item) {
-  //   let modalBody = $(".modal-body");
-  //   let modalTitle = $(".modal-title");
-  //   let modalHeader = $(".modal-header");
-
-  //   modalTitle.empty();
-  //   modalBody.empty();
-
-  //   let nameElement = $("<h1>" + item.name + "</h1>");
-  //   let imageElementFront = $('<img class="modal-img" style="width:50%">');
-  //   imageElementFront.attr("src", item.imageUrlFront);
-  //   let imageElementBack = $('<img class="modal-img" style="width:50%">');
-  //   imageElementBack.attr("src", item.imageUrlBack);
-  //   let heightElement = $("<p>" + "height : " + item.height + "</p>");
-  //   let weightElement = $("<p>" + "weight : " + item.weight + "</p>");
-  //   let typesElement = $("<p>" + "types : " + item.types + "</p>");
-  //   let abilitiesElement = $("<p>" + "abilities : " + item.abilities + "</p>");
-
-  //   modalTitle.append(nameElement);
-  //   modalBody.append(imageElementFront);
-  //   modalBody.append(imageElementBack);
-  //   modalBody.append(heightElement);
-  //   modalBody.append(weightElement);
-  //   modalBody.append(typesElement);
-  //   modalBody.append(abilitiesElement);
-    
-  // }
-
 
   return {
     add: add,
